b20e5ef74035d322aae4ef2c2c5069cd800758ad
Joseph Pong <oxjoe2006@yahoo.com>
2017-07-03 20:09:06 -0400

    finished sequential animation; problem with swtich scenes b/c animation

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,22 @@
+import java.util.concurrent.TimeUnit;
 import javafx.animation.FadeTransition;
-import javafx.animation.Timeline;
+import javafx.animation.SequentialTransition;
 import javafx.application.Application;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import javax.swing.JTextField;
 
 
 public class Main extends Application {
@@ -24,7 +27,7 @@ public class Main extends Application {
 
   @Override
   public void start(Stage primaryStage) {
-    Stage secondryStage = primaryStage;
+    Stage secondaryStage = primaryStage;
     primaryStage.setTitle("Be Productive");
 
     GridPane grid = new GridPane();
@@ -33,18 +36,19 @@ public class Main extends Application {
     grid.setVgap(10);
     grid.setPadding(new Insets(25, 25, 25, 25));
 
-    Text scenetitle = new Text("Are you procrastinating?");
-    scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-    grid.add(scenetitle, 0, 0, 5, 1);
+    Label title = new Label("Are you being productive?");
+    title.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+    grid.add(title, 0, 0, 2,1);
+    grid.setHalignment(title, HPos.CENTER);
+
+    TextField textField = new TextField("no");
+    grid.add(textField, 1, 1);
 
     Button btn = new Button("Submit");
     HBox hbBtn = new HBox(10);
     hbBtn.setAlignment(Pos.CENTER);
     hbBtn.getChildren().add(btn);
-    grid.add(hbBtn, 2, 2);
-
-    TextField textField = new TextField("Yes or No?");
-    grid.add(textField, 2, 1);
+    grid.add(hbBtn, 1, 2);
 
     //    Label userName = new Label("Press Enter");
 //    grid.add(userName, 0, 1);
@@ -54,9 +58,9 @@ public class Main extends Application {
     btn.setOnAction(action -> {
       final String userInput = textField.getText();
       if (userInput.compareToIgnoreCase("yes") == 0) {
-        notGettingStuffDone(secondryStage);
+
       } else if (userInput.compareToIgnoreCase("no") == 0) {
-        //        gettingStuffDone();
+        notGettingStuffDone(secondaryStage);
       } else {
         // Only (Yes/No) is accepted!
       }
@@ -66,22 +70,23 @@ public class Main extends Application {
       if (event.getCode() == KeyCode.ENTER) {
         final String userInput = textField.getText();
         if (userInput.compareToIgnoreCase("yes") == 0) {
-          notGettingStuffDone(secondryStage);
+
         } else if (userInput.compareToIgnoreCase("no") == 0) {
-          //        gettingStuffDone();
+          notGettingStuffDone(secondaryStage);
         } else {
           // Only (Yes/No) is accepted!
         }
       }
     });
 
-    grid.setGridLinesVisible(true);
+  //  grid.setGridLinesVisible(true);
     Scene scene = new Scene(grid, 300, 275);
     primaryStage.setScene(scene);
     primaryStage.show();
   }
 
   public void notGettingStuffDone(Stage secondaryStage) {
+    Stage newStage = secondaryStage;
     //secondaryStage.setTitle("Be Productive");
     GridPane grid = new GridPane();
     grid.setAlignment(Pos.CENTER);
@@ -89,24 +94,86 @@ public class Main extends Application {
     grid.setVgap(10);
     grid.setPadding(new Insets(25, 25, 25, 25));
 
-    Text scenetitle = new Text("That's okay, take a deep breath and count to 3");
-    scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 16));
-    grid.add(scenetitle, 0, 0, 4, 1);
-    FadeTransition ft1 = new FadeTransition(Duration.millis(5000), scenetitle);
-    ft1.setFromValue(1.0);
-    ft1.setToValue(0.1);
-    ft1.setCycleCount(Timeline.INDEFINITE);
-    //ft.setAutoReverse(true);
-    ft1.play();
+    Label t1 = new Label("That's okay");
+    t1.setFont(Font.font("Tahoma", FontWeight.NORMAL, 16));
+    grid.add(t1, 0, 0, 1, 1);
+    grid.setHalignment(t1, HPos.CENTER);
 
-    System.out.println("notGettingStuffDone");
+    Label t2 = new Label("Take a deep breath");
+    t2.setFont(Font.font("Tahoma", FontWeight.NORMAL, 16));
+    grid.add(t2, 0, 1, 1, 1);
+    grid.setHalignment(t2, HPos.CENTER);
 
-    //grid.setGridLinesVisible(true);
+    Label t3 = new Label("and");
+    t3.setFont(Font.font("Tahoma", FontWeight.NORMAL, 16));
+    grid.add(t3, 0, 2, 1, 1);
+    grid.setHalignment(t3, HPos.CENTER);
+
+    Label t4 = new Label("Count to 5");
+    t4.setFont(Font.font("Tahoma", FontWeight.NORMAL, 16));
+    grid.add(t4, 0, 3, 1, 1);
+    grid.setHalignment(t4, HPos.CENTER);
+
+    FadeTransition ft1 = new FadeTransition(Duration.millis(3000), t1);
+    ft1.setFromValue(0);
+    ft1.setToValue(1);
+    ft1.setDuration(Duration.millis(1500));
+    FadeTransition ft2 = new FadeTransition(Duration.millis(2000), t2);
+    ft2.setFromValue(0);
+    ft2.setToValue(1);
+    ft2.setDuration(Duration.millis(1500));
+    FadeTransition ft3 = new FadeTransition(Duration.millis(1500), t3);
+    ft3.setFromValue(0);
+    ft3.setToValue(1);
+    ft3.setDuration(Duration.millis(1000));
+    FadeTransition ft4 = new FadeTransition(Duration.millis(1500), t4);
+    ft4.setFromValue(0);
+    ft4.setToValue(1);
+    ft4.setDuration(Duration.millis(1500));
+
+    Duration one = new Duration(1000);
+    SequentialTransition st = new SequentialTransition();
+    st.getChildren().addAll(ft1, ft2, ft3, ft4);
+    st.play();
+
+    try {
+      TimeUnit.MILLISECONDS.sleep(1500);
+      grid.getChildren().removeAll(t1,t2,t3,t4);
+      readyScene(newStage);
+    } catch (InterruptedException e) {
+    }
+
+
+    // grid.setGridLinesVisible(true);
 
     Scene scene = new Scene(grid, 300, 275);
     secondaryStage.setScene(scene);
     secondaryStage.show();
+    //
+  }
 
+  public void readyScene(Stage newStage){
+    System.out.println("MADDE IT");
+    GridPane grid = new GridPane();
+    grid.setAlignment(Pos.CENTER);
+    grid.setHgap(10);
+    grid.setVgap(10);
+    grid.setPadding(new Insets(25, 25, 25, 25));
+
+    Label t1 = new Label("That's okay");
+    t1.setFont(Font.font("Tahoma", FontWeight.NORMAL, 16));
+    grid.add(t1, 0, 0, 1, 1);
+    grid.setHalignment(t1, HPos.CENTER);
+
+    Label t2 = new Label("Take a deep breath");
+    t2.setFont(Font.font("Tahoma", FontWeight.NORMAL, 16));
+    grid.add(t2, 0, 1, 1, 1);
+    grid.setHalignment(t2, HPos.CENTER);
+
+
+    Scene scene = new Scene(grid, 300, 275);
+    newStage.setScene(scene);
+    newStage.show();
   }
 
 }
